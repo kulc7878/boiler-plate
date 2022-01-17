8bbbf7d55922d6bf225dbcc5d76aa1c9ad8aa4bc
jongjin <kulc78@vntgcorp.com>
2022-01-17 19:47:08 +0900

    config 생성

--- index.js
+++ index.js
@@ -2,14 +2,38 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//applicatino/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://boiler:plate@boiler-plate.12ejy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true //, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MogoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 새해복 많이~')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입 정보를 받아온다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  }) 
 })
 
 app.listen(port, () => {
